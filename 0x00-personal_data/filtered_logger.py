35f44e6997a42b3ba17b88f72fd75f69a8c31e70
SaidLamghari <said.lmg@gmail.com>
2024-08-01 13:17:19 +0000

    pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -90,16 +90,21 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
-    Establish and return a connection to the MySQL database using environment variables.
+    Establish and return a connection to the MySQL
+    database using environment variables.
 
-    This function retrieves database connection credentials from environment variables and
-    uses them to connect to the MySQL database. The connection object is then returned.
+    This function retrieves database connection
+    credentials from environment variables and
+    uses them to connect to the MySQL database.
+    The connection object is then returned.
 
     Returns:
-        mysql.connector.connection.MySQLConnection: The database connection object.
-    
+        mysql.connector.connection.MySQLConnection:
+        The database connection object.
+
     Raises:
-        mysql.connector.Error: If there is an error connecting to the database.
+        mysql.connector.Error: If there is an error
+        connecting to the database.
     """
     # Retrieve database credentials from
     environment variables with default values
