3f7a935b842a5c75984b4b914cceb6a961e4649a
SaidLamghari <said.lmg@gmail.com>
2024-08-08 19:00:28 +0000

    api/v1/views/session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,86 +1,102 @@
 #!/usr/bin/env python3
 """
-Module des vues d'authentification par session.
-Auteuur SAID LAMGHARI
+Module de vues pour
+l'authentification par session.
+Auteur SAID LAMGHARI
 """
 import os
 from typing import Tuple
-from api.v1.app import auth
-
 from flask import abort
-from flask import jsonify, request
-from models.user import User
+from flask import jsonify
+from flask import request
 from api.v1.views import app_views
+from models.user import User
 
 
 @app_views.route('/auth_session/login',
                  methods=['POST'], strict_slashes=False)
-def for_login() -> Tuple[str, int]:
-    """POST /api/v1/auth_session/login
-    Cette route permet à un utilisateur de se connecter
-    en fournissant un email et un mot de passe.
-
-    Retourne :
-      - Une réponse JSON représentant l'objet utilisateur
-      si l'authentification est réussie.
-      - 400 si l'email ou le mot de passe est manquant.
-      - 404 si aucun utilisateur n'est trouvé avec cet email.
-      - 401 si le mot de passe est incorrect.
+def login() -> Tuple[str, int]:
+    """Route POST pour /api/v1/auth_session/login
+    Authentifie un utilisateur et crée une
+    session si les informations sont valides.
+    
+    Retourne:
+      - La représentation JSON d'un objet User en cas de succès.
+      - Une réponse d'erreur avec un code
+      d'état approprié en cas d'échec.
     """
+    # Message de réponse pour
+    # l'absence d'utilisateur avec cet email
     no_rslt = {"error": "no user found for this email"}
-
-    # Récupère l'email et le mot de passe de la requête
+    
+    # Récupération de l'email et du mot de
+    # passe depuis le formulaire de la requête
     email = request.form.get('email')
-    if email is None or len(email.strip()) == 0:
-        # Retourne une erreur si l'email est manquant ou vide
+    var_lenemail = len(email.strip())
+    if email is None or var_lenemail == 0:
+        # Retourne une erreur 400 si l'email est manquant ou vide
         return jsonify({"error": "email missing"}), 400
-
+    
     password = request.form.get('password')
-    if password is None or len(password.strip()) == 0:
-        # Retourne une erreur si le mot de passe est manquant ou vide
+    var_lenpass = len(password.strip())
+    if password is None or var_lenpass == 0:
+        # Retourne une erreur 400 si
+        # le mot de passe est manquant ou vide
         return jsonify({"error": "password missing"}), 400
-
+    
     try:
-        # Recherche les utilisateurs par email
+        # Recherche d'un utilisateur avec l'email fourni
         users = User.search({'email': email})
     except Exception:
-        # Retourne une erreur 404 en cas d'exception durant la recherche
+        # Retourne une erreur 404 en
+        # cas d'exception durant la recherche
         return jsonify(no_rslt), 404
-
-    if len(users) <= 0:
-        # Retourne une erreur 404 si aucun utilisateur n'est trouvé
+    
+    # Vérifie si aucun utilisateur n'a été trouvé
+    var_len = len(users)
+    if var_len <= 0:
         return jsonify(no_rslt), 404
-
+    
+    # Vérifie si le mot de passe fourni est valide
     if users[0].is_valid_password(password):
-        # Si le mot de passe est valide,
-        # crée une session et retourne l'utilisateur
-        session_id = auth.create_session(getattr(users[0], 'id'))
+        # Importation de l'objet
+        # d'authentification depuis l'application
+        from api.v1.app import auth
+        
+        # Création d'une session et récupération de l'ID de session
+        sssionid = auth.create_session(getattr(users[0], 'id'))
+        
+        # Création de la réponse JSON avec les informations de l'utilisateur
         rslt = jsonify(users[0].to_json())
-        # Définit le cookie de session dans la réponse
-        rslt.set_cookie(os.getenv("SESSION_NAME"), session_id)
+        
+        # Définition du cookie de session avec l'ID de session
+        rslt.set_cookie(os.getenv("SESSION_NAME"), sssionid)
+        
         return rslt
-
+    
     # Retourne une erreur 401 si le mot de passe est incorrect
     return jsonify({"error": "wrong password"}), 401
 
-
-@app_views.route('/auth_session/logout',
-                 methods=['DELETE'], strict_slashes=False)
-def for_logout() -> Tuple[str, int]:
-    """DELETE /api/v1/auth_session/logout
-    Cette route permet à un utilisateur de
-    se déconnecter en détruisant la session.
-
-    Retourne :
-      - Un objet JSON vide si la déconnexion est réussie.
-      - 404 si la session ne peut pas être détruite.
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> Tuple[str, int]:
+    """Route DELETE pour /api/v1/auth_session/logout
+    Déconnecte l'utilisateur en détruisant la session active.
+    
+    Retourne:
+      - Un objet JSON vide en cas de succès.
+      - Une erreur 404 si la destruction de la session échoue.
     """
-    # Tente de détruire la session de l'utilisateur
-    for_delet = auth.destroy_session(request)
-    if not for_delet:
-        # Retourne une erreur 404 si la
-        # session ne peut pas être détruite
+    # Importation de l'objet d'authentification depuis l'application
+    from api.v1.app import auth
+    
+    # Tentative de destruction de la session active
+    idestroyed = auth.destroy_session(request)
+    
+    # Retourne une erreur 404 si
+    # la session n'a pas pu être détruite
+    if not idestroyed:
         abort(404)
-
-    # Retourne un objet JSON vide si la déconnexion est réussie
+    
+    # Retourne un objet JSON vide en cas de succès
     return jsonify({})
