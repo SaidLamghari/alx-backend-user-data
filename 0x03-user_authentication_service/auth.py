f3ea76cf5ee016c194c5736d02e5ef4e349c5b7b
SaidLamghari <said.lmg@gmail.com>
2024-08-15 19:19:00 +0000

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,113 +1,236 @@
 #!/usr/bin/env python3
 """
-Ce module contient la classe Auth,
-responsable de la gestion
-de l'authentification des utilisateurs.
+Ce module implémente les routines liées
+à l'authentification des utilisateurs,
+y compris l'enregistrement, la gestion des sessions,
+et la réinitialisation des mots de passe.
+Auteur SAID LAMGHARI
 """
-
-import uuid
-from db import DB
 from user import User
-from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
-from typing import Optional
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+
+
+def passwordforhash(pssword: str) -> bytes:
+    """
+    Hache un mot de
+    passe en utilisant bcrypt.
+
+    Arguments:
+        - password (str): Le mot de
+        passe en texte clair.
+
+    Retourne:
+        - bytes: Le mot de passe
+        haché sous forme de bytes.
+    """
+    return bcrypt.hashpw(pssword.encode("utf-8"), bcrypt.gensalt())
 
 
 class Auth:
     """
-    Auth gère les opérations d'authentification telles que
-    l'inscription, la connexion,
-    et la gestion des mots de passe.
+    La classe Auth gère les opérations
+    d'authentification des utilisateurs,
+    y compris l'enregistrement, la validation
+    des identifiants de connexion,
+    la gestion des sessions et la
+    réinitialisation des mots de passe.
     """
 
-    def __init__(self) -> None:
-        """Initialise l'instance Auth avec
-        une instance de la base de données."""
+    def __init__(self):
+        """
+        Initialise une nouvelle instance
+        de la classe Auth.
+
+        Cette méthode initialise une connexion
+        à la base de données en instanciant
+        la classe DB.
+        """
         self._db = DB()
 
-    def register_user(self, email: str,
-                      password: str) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """
         Enregistre un nouvel utilisateur
-        avec un email et un mot de passe.
-        Retourne l'utilisateur enregistré.
+        dans la base de données.
+
+        Cette méthode vérifie d'abord si l'email
+        existe déjà. Si non, elle crée un nouvel
+        utilisateur avec l'email et le mot de passe haché.
+
+        Arguments:
+            - email (str): L'email de
+            l'utilisateur à enregistrer.
+            - password (str): Le mot de passe
+            en texte clair de l'utilisateur.
+
+        Retourne:
+            - User: L'objet User nouvellement créé.
+
+        Lève:
+            - ValueError: Si l'utilisateur
+            avec cet email existe déjà.
         """
         try:
+            # Vérifie si l'utilisateur existe déjà
             self._db.find_user_by(email=email)
-            raise ValueError
         except NoResultFound:
-            hashed_pw = bcrypt.hashpw(password.encode('utf-8'),
-                                      bcrypt.gensalt())
-            user = self._db.add_user(email, hashed_pw)
-            return user
+            # Ajoute l'utilisateur si non trouvé
+            return self._db.add_user(email, passwordforhash(password))
+        # Lève une exception si l'utilisateur existe déjà
+        raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
         """
-        Vérifie si l'email et le mot de passe sont valides.
-        Retourne True si la connexion est réussie, sinon False.
+        Vérifie si les détails de connexion
+        d'un utilisateur sont valides.
+
+        Cette méthode hache le mot de passe
+        fourni et le compare avec le mot de passe
+        haché stocké dans la base de données.
+
+        Arguments:
+            - email (str): L'email de l'utilisateur.
+            - password (str): Le mot de passe en texte clair.
+
+        Retourne:
+            - bool: True si les identifiants
+            sont valides, False sinon.
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
+            if user is not None:
+                return bcrypt.checkpw(password.encode("utf-8"),
+                                      user.hashed_password)
         except NoResultFound:
-            return False
+            return False  # Retourne False si aucun utilisateur n'est trouvé
+        return False  # Retourne False par défaut si la vérification échoue
 
-    def create_session(self, email: str) -> str:
+    def create_session(self, email: str) -> Union[str, None]:
         """
-        Crée une session utilisateur et retourne l'ID de session.
-        """
-        user = self._db.find_user_by(email=email)
-        session_id = str(uuid.uuid4())
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
+        Crée une nouvelle session pour un utilisateur.
 
-    def get_user_from_session_id(self,
-                                 session_id: Optional[str]) -> Optional[User]:
+        Cette méthode génère un identifiant
+        de session unique (UUID) et l'associe
+        à l'utilisateur correspondant à l'email fourni.
+
+        Arguments:
+            - email (str): L'email de l'utilisateur.
+
+        Retourne:
+            - str: L'ID de session nouvellement créé.
+            - None: Si l'utilisateur n'existe pas.
         """
-        Retourne l'utilisateur correspondant à l'ID de session.
-        Retourne None si la session n'est pas trouvée.
+        try:
+            varuser = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None  # Retourne None si l'utilisateur n'est pas trouvé
+        sessid = str(uuid4())  # Génère un UUID pour la session
+        # Met à jour l'utilisateur avec le nouvel ID de session
+        self._db.update_user(varuser.id, session_id=sessid)
+        return sessid
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """
+        Récupère un utilisateur basé sur un ID de session donné.
+
+        Arguments:
+            - session_id (str): L'ID de session de l'utilisateur.
+
+        Retourne:
+            - User: L'objet User correspondant à l'ID de session.
+            - None: Si aucun utilisateur n'est
+            trouvé ou si l'ID de session est invalide.
         """
         if session_id is None:
+            # Retourne None si l'ID de session est None
             return None
         try:
-            user = self._db.find_user_by(session_id=session_id)
-            return user
+            # Recherche l'utilisateur par ID de session
+            return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
+            # Retourne None si aucun utilisateur n'est trouvé
             return None
 
     def destroy_session(self, user_id: int) -> None:
         """
-        Détruit la session de l'utilisateur spécifié.
+        Détruit une session associée à un utilisateur donné.
+
+        Cette méthode supprime l'ID de session de l'utilisateur,
+        ce qui équivaut à déconnecter l'utilisateur.
+
+        Arguments:
+            - user_id (int): L'ID de l'utilisateur
+            dont la session doit être détruite.
         """
+        if user_id is None:
+            return None  # Ne rien faire si l'ID de l'utilisateur est None
+        # Met à jour l'utilisateur en supprimant l'ID de session
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
         """
-        Génère un token de réinitialisation
-        de mot de passe pour l'utilisateur.
-        Retourne le token généré.
+        Génère un jeton de réinitialisation
+        de mot de passe pour un utilisateur.
+
+        Cette méthode génère un UUID pour la
+        réinitialisation du mot de passe et
+        l'associe à l'utilisateur
+        correspondant à l'email fourni.
+
+        Arguments:
+            - email (str): L'email de l'utilisateur.
+
+        Retourne:
+            - str: Le jeton de réinitialisation
+            de mot de passe.
+
+        Lève:
+            - ValueError: Si aucun utilisateur n'est trouvé pour cet email.
         """
         try:
-            user = self._db.find_user_by(email=email)
-            resettoken = str(uuid.uuid4())
-            self._db.update_user(user.id,
-                                 reset_token=resettoken)
-            return resettoken
+            varuser = self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError
+            # Lève une exception si aucun utilisateur n'est trouvé
+            raise ValueError("User not found")
+        # Génère un UUID pour la réinitialisation
+        resetoken = str(uuid4())
+        # Met à jour l'utilisateur avec le nouveau jeton
+        self._db.update_user(varuser.id, reset_token=resetoken)
+        return resetoken
 
-    def update_password(self, reset_token: str,
-                        password: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         """
-        Met à jour le mot de passe de l'utilisateur
-        en utilisant le token de réinitialisation.
+        Met à jour le mot de passe d'un utilisateur
+        donné un jeton de réinitialisation valide.
+
+        Cette méthode recherche l'utilisateur par
+        son jeton de réinitialisation, hache
+        le nouveau mot de passe,
+        et met à jour la base de données.
+
+        Arguments:
+            - reset_token (str): Le jeton de
+            réinitialisation de l'utilisateur.
+            - password (str): Le nouveau mot
+            de passe en texte clair.
+
+        Lève:
+            - ValueError: Si aucun utilisateur
+            n'est trouvé pour ce jeton.
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_pw = bcrypt.hashpw(password.encode('utf-8'),
-                                      bcrypt.gensalt())
-            self._db.update_user(user.id,
-                                 hashed_password=hashed_pw, reset_token=None)
         except NoResultFound:
-            raise ValueError
+            # Lève une exception si le jeton est invalide
+            raise ValueError("Invalid reset token")
+        # Hache le nouveau mot de passe
+        nwpsswordforhash = passwordforhash(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=nwpsswordforhash,
+            # Supprime le jeton de réinitialisation après utilisation
+            reset_token=None,
+        )
